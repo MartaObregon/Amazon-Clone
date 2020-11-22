807e0574da1f0cc18c9d1c4ae8c93454b718fa21
Marta Obregon <marta.garcia.obregon@.com>
2020-11-22 16:38:43 +0000

    set up

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAnaH_nafWoLvIMic7IHTMEv4KCBR-Owqo",
+    authDomain: "clone-c1d79.firebaseapp.com",
+    databaseURL: "https://clone-c1d79.firebaseio.com",
+    projectId: "clone-c1d79",
+    storageBucket: "clone-c1d79.appspot.com",
+    messagingSenderId: "1000205380730",
+    appId: "1:1000205380730:web:c83ee087a138b6b42ddfc3",
+    measurementId: "G-Z4FSBQ7DJM"
+  };
