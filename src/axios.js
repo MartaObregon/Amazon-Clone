d7500273d838b0fcfcae1af6baff94967ad0eebf
Marta Obregon <marta.garcia.obregon@.com>
2020-11-25 18:51:56 +0000

    deploy2

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-c1d79/us-central1/api' //the API (cloud function) URL
+    baseURL: 'https://us-central1-clone-c1d79.cloudfunctions.net/api' //the API (cloud function) URL
 })
 
 export default instance;
+
+//http://localhost:5001/clone-c1d79/us-central1/api
